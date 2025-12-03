911478be1a4714788605c05d6ef3540ce71f3f1f
Renier Clyde <clyde.renier02gmail.com>
2025-12-03 11:38:35 +0100

    fin Ex10

---
+++ Ex10/script.js
@@ -0,0 +1,19 @@
+let produits = [
+    { nom: "Stylo", prix: 1.5 },
+    { nom: "Cahier", prix: 3 },
+    { nom: "Gomme", prix: 0.8 }
+];
+
+let filtre = [];
+
+function SupérieurA1Euro() {
+
+    for(let i = 0; i < produits.length; i++) {
+        if(produits[i].prix > 1) {
+            filtre.push(produits[i]);
+        }
+    }
+    return filtre;
+}
+
+console.log(SupérieurA1Euro());
