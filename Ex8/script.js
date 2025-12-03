9748b8ed9036048c64caebecfa4a6150f4c08a55
Renier Clyde <clyde.renier02gmail.com>
2025-12-03 09:33:27 +0100

    fin Ex8

---
+++ Ex8/script.js
@@ -0,0 +1,16 @@
+let personnes = ["alice", "bob", "claire"];
+const ul = document.getElementById("listeNoms");
+
+function afficherNomsMajuscules() {
+
+    for(let i = 0; i < personnes.length; i++) {
+        let nom = personnes[i];
+        let nomMaj = nom.toUpperCase();
+
+        const li = document.createElement("li");
+        li.textContent = nomMaj;
+        ul.appendChild(li);
+    }
+}
+
+console.log(afficherNomsMajuscules());
